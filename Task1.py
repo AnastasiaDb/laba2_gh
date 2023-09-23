a6b9d9fba1b2830461e5abf97a7126138d6e9313
Anastasia <82052788+AnastasiaDb@users.noreply.github.com>
2023-09-23 21:31:14 +0300

    исправила 1 задание

---
+++ Task1.py
@@ -0,0 +1,57 @@
+import math
+from time import sleep
+
+import numpy as np
+from PIL import Image
+from matplotlib import pyplot as plt
+
+
+def task1():
+    max_diff = -1
+    min_diff = 256
+    image = Image.open("color.jpg")
+    w, h = image.size
+    myImage1 = Image.new("RGB", (w, h))
+    myImage2 = Image.new("RGB", (w, h))
+    diffImage = Image.new("RGB", (w, h))
+    matrix = np.zeros((w, h), dtype=int)  # Создание двумерного массива с нулевыми значениями
+
+    for x in range(w):
+        for y in range(h):
+            r, g, b = image.getpixel((x, y))
+
+            val1 = int(0.299 * r + 0.587 * g + 0.114 * b)
+            val2 = int(0.2126 * r + 0.7152 * g + 0.0722 * b)
+            myImage1.putpixel((x, y), (val1,val1,val1))
+            myImage2.putpixel((x, y), (val2,val2,val2))
+            # diffImage.putpixel((x, y), abs(val1-val2))
+            diff = val1 - val2
+            matrix[x][y] = diff
+            if diff > max_diff:
+                max_diff = diff
+            if diff < min_diff:
+                min_diff = diff
+    #print (max_diff,'  ',min_diff)
+    for x in range(w):
+        for y in range(h):
+            pix_diff = int(matrix[x][y] * 255 / (max_diff - min_diff))
+            diffImage.putpixel((x, y), (pix_diff,pix_diff,pix_diff))
+    #myImage1.show()  # save("1.jpg")
+    # sleep(3)
+    #myImage2.show()  # save("2.jpg")
+    # sleep(3)
+    #diffImage.show()  # save("3.jpg")
+
+    rgbIm = np.concatenate((myImage1, myImage2, diffImage), axis=0)
+    plt.imshow(rgbIm)
+    plt.axis('off')
+    plt.show()
+    # plt.show()
+    # fig, axs = plt.subplots()
+    # axs.stairs(myImage1.histogram(), label='Type 1')
+    # axs.stairs(myImage2.histogram(), label='Type 2')
+    # axs.set_title("Использование тонов")
+    # axs.legend()
+    # plt.show()
+
+task1()
